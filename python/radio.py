9c6679b55b2ac1aca6407ff429a4bc434dce9d93
Nick Foster <nick@ettus.com>
2013-11-13 22:16:26 -0800

    Strip out unused imports, remove dual-tuner UHD code

--- python/radio.py
+++ python/radio.py
@@ -23,15 +23,11 @@
 # You pass it options, it gives you data.
 # It uses the pubsub interface to allow clients to subscribe to its data feeds.
 
-from gnuradio import gr, gru, eng_notation, filter, blocks, digital, analog, audio, uhd
+from gnuradio import gr, blocks, uhd
 from gnuradio.filter import pfb
 from gnuradio.eng_option import eng_option
 from gnuradio.gr.pubsub import pubsub
 from optparse import OptionParser, OptionGroup
-import threading
-import time
-import sys
-import re
 import scanner
 
 class filterbank(gr.hier_block2):
@@ -170,15 +166,7 @@ class trunked_feed(gr.hier_block2, pubsub):
             return float(master_rate) / self._channel_decimation
 
     def set_center_freq(self, freq):
-        if self._source_name == "uhd":
-            tr = uhd.tune_request_t()
-            tr.rf_freq_policy = uhd.tune_request_t.POLICY_MANUAL
-            tr.dsp_freq_policy = uhd.tune_request_t.POLICY_MANUAL
-            tr.rf_freq = freq
-            tr.target_freq = freq
-            tune_result = self._data_src.set_center_freq(tr,0)
-            self._freqs["center"] = tune_result.actual_rf_freq
-        elif self._source_name in ("hackrf", "rtlsdr"):
+        if self.live_source():
             self._data_src.set_center_freq(freq)
             self._freqs["center"] = self._data_src.get_center_freq()
 
@@ -188,28 +176,14 @@ class trunked_feed(gr.hier_block2, pubsub):
     def set_freq(self, chan, freq):
         print "Setting %s to %.4fMHz" % (chan, freq/1.e6)
         chan_num = ["ctrl", "audio"].index(chan)
-        if self._source_name == "uhd":
-            tr = uhd.tune_request_t()
-            tr.rf_freq_policy = uhd.tune_request_t.POLICY_MANUAL
-            tr.dsp_freq_policy = uhd.tune_request_t.POLICY_MANUAL
-            tr.dsp_freq = self._freqs["center"] - freq
-            tr.rf_freq = self._freqs["center"]
-            tr.target_freq = self._freqs["center"]
-            tune_result = self._data_src.set_center_freq(tr, chan_num)
-            print "Center frequency: %.4fMHz" % (tune_result.actual_rf_freq/1.e6)
-            print "DSP frequency: %.4fMHz" % (tune_result.actual_dsp_freq/1.e6)
-            print "%s channel: %.4fMHz" % (chan,
-                                        ((tune_result.actual_rf_freq - tune_result.actual_dsp_freq)/1.e6)
-                                        )
-        else:
-            #find the channel number
-            offset = freq - self._freqs["center"]
-            if abs(offset) > (self.get_rate("master") / 2):
-                #TODO: handle this by retuning the center/operating single-channel
-                print "Asked for a channel outside the band"
-                return
-            self._filter_bank.set_freq(chan_num, offset)
-            print "Setting channel %i" % (chan_num)
+        #find the channel number
+        offset = freq - self._freqs["center"]
+        if abs(offset) > (self.get_rate("master") / 2):
+            #TODO: handle this by retuning the center/operating single-channel
+            print "Asked for a channel outside the band"
+            return
+        self._filter_bank.set_freq(chan_num, offset)
+        print "Setting channel %i" % (chan_num)
 
         self._freqs[chan] = freq
 
@@ -250,99 +224,4 @@ class trunked_feed(gr.hier_block2, pubsub):
         group.add_option("-r", "--rate", type="eng_float", default=None,
                          help="sample rate, leave blank for automatic")
         parser.add_option_group(group)
-
-class audio_path (gr.hier_block2, pubsub):
-    def __init__(self, options):
-        gr.hier_block2.__init__(self,
-                                "audio_path",
-                                gr.io_signature(1,1,gr.sizeof_gr_complex),
-                                gr.io_signature(0,0,gr.sizeof_gr_complex))
-        pubsub.__init__(self)
-        self._options = options
-        self.audiorate = options.audio_rate
-        self.prefilter_decim = float(options.rate) / self.audiorate
-        print "Audio demod decimation: %f" % self.prefilter_decim
-        self.audio_prefilter = filter.pfb.arb_resampler_ccf(1.0/self.prefilter_decim)
-
-        #on a trunked network where you know you will have good signal,
-        #a carrier power squelch works well. real FM receivers use a
-        #noise squelch, where the received audio is high-passed above
-        #the cutoff and then fed to a reverse squelch. If the power is
-        #then BELOW a threshold, open the squelch.
-        self.squelch = analog.pwr_squelch_cc(options.squelch, #squelch point
-                                            alpha = 0.1, #wat
-                                            ramp = 10, #wat
-                                            gate = False)
-
-        self.audiodemod = analog.fm_demod_cf(self.audiorate, #rate
-                            1, #audio decimation
-                            4000, #deviation
-                            3000, #audio passband
-                            4000, #audio stopband
-                            options.volume, #gain
-                            75e-6) #deemphasis constant
-
-        #the filtering removes FSK data woobling from the subaudible channel
-        self.audiofilttaps = filter.firdes.high_pass(1, self.audiorate, 300,
-                                                        50, filter.firdes.WIN_HANN)
-        self.audiofilt = filter.fft_filter_fff(1, self.audiofilttaps)
-        self.audiosink = audio.sink(int(self.audiorate), "")
-
-        self.connect(self, self.audio_prefilter, self.squelch, self.audiodemod, self.audiofilt, self.audiosink)
-
-    @staticmethod
-    def add_options(parser):
-        group = OptionGroup(parser, "Audio setup options")
-        group.add_option("-v", "--volume", type="eng_float", default=2.5,
-                            help="set volume gain for audio output [default=%default]")
-        group.add_option("-s", "--squelch", type="eng_float", default=-40,
-                            help="set audio squelch level in dBfs(default=%defaultdB, play with it)")
-        group.add_option("-a", "--audio-rate", type="eng_float", default=48e3,
-                            help="set audio rate [default=%default]")
-        parser.add_option_group(group)
-
-#instantiates a sample feed, control decoder, audio sink, and control
-class scanner_radio (gr.top_block, pubsub):
-    def __init__(self, options):
-        gr.top_block.__init__(self)
-        pubsub.__init__(self)
-        self._options = options
-
-        self._feed = trunked_feed(options)
-        self._monitor = [int(i) for i in options.monitor.split(",")]
-        self._tg_assignments = {}
-
-        if options.type == 'smartnet':
-            self._data_path = scanner.smartnet_ctrl_rx(self._feed.get_rate("ctrl"))
-        elif options.type == 'edacs':
-            self._data_path = scanner.edacs_ctrl_rx(self._feed.get_rate("ctrl"))
-        else:
-            raise Exception("Invalid network type (must be edacs or smartnet)")
-        self.connect((self._feed,0), self._data_path)
-        options.rate = self._feed.get_rate("audio")
-        self._audio_path = audio_path(options)
-        self.connect((self._feed,1), self._audio_path)
-
-        #setup a callback to retune the audio feed freq
-        self._data_path.set_assign_callback(self.handle_assignment)
-
-    def handle_assignment(self, addr, groupflag, freq):
-        #TODO handle all channel assignment and priority monitor stuff here
-        if (addr & 0xFFF0) in self._monitor:# and self._tg_assignments.get(addr) != freq: #mask because last 4 bits is priority mask
-            self._feed.set_audio_freq(freq)
-
-        self._tg_assignments[addr] = freq
-
-    @staticmethod
-    def add_options(parser):
-        trunked_feed.add_options(parser)
-        audio_path.add_options(parser)
-        group = OptionGroup(parser, "Scanner setup options")
-        #Choose source
-        group.add_option("-m","--monitor", type="string", default="0",
-                        help="Monitor a list of talkgroups (comma-separated) [default=%default]")
-        group.add_option("-t","--type", type="string", default="smartnet",
-                         help="Network type (edacs or smartnet only) [default=%default]")
-        parser.add_option_group(group)
-
 
